cfbba2cd2ddfae25893d5807abfb334679184472
임성우 <seongwoorang@naver.com>
2022-01-27 23:09:45 +0900

    인사말 출력 및 사용자 이름 받아서 출력

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    /*
+    Step 1. 인사말 출력
+    Step 2. 사용자 이름 받기
+    Step 3. 입력받은 사용자 이름 출력
+     */
+    println("안녕! 코틀린 정복하자")
+    println("너의 이름은? : ")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val name = readLine()
+    println("""
+        반가워요 $name 님
+        원하시는 카테고리를 입력해주세요.
+        ===============================***
+    """.trimIndent())
+
 }
